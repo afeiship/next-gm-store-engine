ce5ebf0ab706a69dfae1ec4636863ad2f17581fb
afeiship <1290657123@qq.com>
2021-01-06 22:10:04 +0800

    Release 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,13 @@
  * name: @jswork/next-gm-store-engine
  * description: Store engin for tampermonkey GM_storage.
  * homepage: https://github.com/afeiship/next-gm-store-engine
- * version: 1.0.1
- * date: 2020-11-20 16:45:58
+ * version: 1.0.2
+ * date: 2021-01-06 22:09:56
  * license: MIT
  */
 
 (function() {
-  var global = global || this || window || Function('return this')();
+  var global = global || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
 
   var NxGmStoreEngine = nx.declare('nx.GmStoreEngine', {
